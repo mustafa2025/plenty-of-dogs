ac194f0284b63b73bbe5e26dac12428dc9e7b0f9
Mustafa Ahmed <m.awes780@gmail.com>
2022-12-22 22:03:08 -0600

    dogs

--- app/GlobalNav.tsx
+++ app/GlobalNav.tsx
@@ -9,43 +9,69 @@ export default function GlobalNav() {
   const [selectedLayoutSegments] = useSelectedLayoutSegments();
 
   return (
-    <div className="space-y-5">
-      {demos.map((demo) => {
-        return (
-          <div key={demo.name}>
-            <div className="mb-2 px-3 text-xs font-semibold uppercase tracking-wider text-zinc-500">
-              <div>{demo.name}</div>
-            </div>
+    <>
+      <nav className="w-1/4">
+        <ul className="max-w-800 flex">
+          <li className="p-4">
+            <Link href="/">
+              <a>Pupster</a>
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link href="/">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link href="/discover">
+              <a>Discover</a>
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link href="/search">
+              <a>Search</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+    // <div className="space-y-5">
+    //   {demos.map((demo) => {
+    //     return (
+    //       <div key={demo.name}>
+    //         <div className="mb-2 px-3 text-xs font-semibold uppercase tracking-wider text-zinc-500">
+    //           <div>{demo.name}</div>
+    //         </div>
 
-            {demo.items.map((item) => {
-              const isActive = item.slug === selectedLayoutSegments;
+    //         {demo.items.map((item) => {
+    //           const isActive = item.slug === selectedLayoutSegments;
 
-              return (
-                <div key={item.slug}>
-                  {item.isDisabled ? (
-                    <div
-                      className="block rounded-md px-3 py-2 text-sm font-medium text-zinc-600"
-                      title="Coming Soon"
-                    >
-                      {item.name}
-                    </div>
-                  ) : (
-                    <Link
-                      href={`/${item.slug}`}
-                      className={clsx(
-                        'block rounded-md px-3 py-2 text-sm font-medium hover:bg-zinc-800 hover:text-zinc-100',
-                        { 'text-zinc-400': !isActive, 'text-white': isActive },
-                      )}
-                    >
-                      {item.name}
-                    </Link>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+    //           return (
+    //             <div key={item.slug}>
+    //               {item.isDisabled ? (
+    //                 <div
+    //                   className="block rounded-md px-3 py-2 text-sm font-medium text-zinc-600"
+    //                   title="Coming Soon"
+    //                 >
+    //                   {item.name}
+    //                 </div>
+    //               ) : (
+    //                 <Link
+    //                   href={`/${item.slug}`}
+    //                   className={clsx(
+    //                     'block rounded-md px-3 py-2 text-sm font-medium hover:bg-zinc-800 hover:text-zinc-100',
+    //                     { 'text-zinc-400': !isActive, 'text-white': isActive },
+    //                   )}
+    //                 >
+    //                   {item.name}
+    //                 </Link>
+    //               )}
+    //             </div>
+    //           );
+    //         })}
+    //       </div>
+    //     );
+    //   })}
+    // </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,32 +2,60 @@ import '@/styles/globals.css';
 import React from 'react';
 import AddressBar from '@/ui/AddressBar';
 import GlobalNav from './GlobalNav';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const image = 'https://picsum.photos/id/1025/600/400/';
   return (
     <html>
       <head>
         <title>Next.js Turbopack App Directory Playground</title>
       </head>
-      <body className="overflow-y-scroll bg-zinc-900">
-        <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8">
+      <body className="overflow-y-scroll ">
+        <div className="">
           <div className="col-start-2">
             <GlobalNav />
           </div>
 
           <div className="col-start-3 space-y-6">
-            <AddressBar />
-
-            <div className="rounded-xl border border-zinc-800 bg-black p-8">
-              {children}
+            <div
+              className="p-12 text-center"
+              style={{
+                backgroundImage: `url(${image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+              <h1 className="mb-4 text-4xl font-bold text-white">Pupster</h1>
+              <p className="mb-8 text-white">They're the good boys and girls</p>
             </div>
+
+            {/* <AddressBar /> */}
+
+            {/* <div className="rounded-xl border border-zinc-800 bg-black p-8">
+              {children}
+            </div> */}
+          </div>
+          <div>
+            <h1 className="p-6 text-4xl font-bold">Welcome to Pupster</h1>
+            <p className="pl-6">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+              maxime a dolorem ullam. Inventore reiciendis nisi repellat,
+              accusantium ducimus nulla delectus dolores laborum repudiandae,
+              deserunt neque explicabo voluptate libero fuga? Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Labore quasi et vitae
+              tenetur similique, iusto assumenda sapiente repellat nulla rem
+              quisquam magni eaque blanditiis vel harum tempore, itaque autem
+              inventore?
+            </p>
           </div>
 
-          <div className="col-start-3 col-end-4 mt-28 flex items-center justify-center">
+          {/* <div className="col-start-3 col-end-4 mt-28 flex items-center justify-center">
             <div className="text-sm text-zinc-600">
               Created by the <b>Next.js</b>
               {' team at '}
@@ -50,7 +78,7 @@ export default function RootLayout({
               </a>
               {'.'}
             </div>
-          </div>
+          </div> */}
         </div>
       </body>
     </html>
